77e67af1e6d6b4ef081ac32e093831f6c63f2098
Justo Leon <franciscoleonpastor@gmail.com>
2022-10-20 02:49:33 -0500

    Merge remote-tracking branch 'origin/master'

--- t00_antecedentes/t00_antecedentes/Script06 - impresiones.py
+++ t00_antecedentes/t00_antecedentes/Script06 - impresiones.py
@@ -1,1 +1,12 @@
-libro_fav = ""
+libro_fav = "la Revelación"
+autor_fav = "Gerald Messadie"
+
+favs = "Mi libro favorito es " + libro_fav + " de " + autor_fav
+print(favs)
+
+libro_fav = input("¿Cuál es tu libro favorito?")
+autor_fav = input("¿Cual es el autor? ")
+print("Tu libro favorito es: {}, del autor: {}".format(libro_fav, autor_fav))
+
+edad = int(input('¿Cuál es tu edad? '))
+print
