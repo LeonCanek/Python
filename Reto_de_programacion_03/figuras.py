1d66d549015d1c71331ff637715df7e1e5efddda
Justo Leon <franciscoleonpastor@gmail.com>
2022-10-22 18:26:40 -0500

    Merge remote-tracking branch 'origin/master'

--- Reto_de_programacion_03/figuras.py
+++ Reto_de_programacion_03/figuras.py
@@ -1,12 +1,12 @@
-from figuras2d.Punto import Punto
-from figuras2d.Circunferencia import Circunferencia
+from figuras2d.Punto import Punto as Rosa
+from figuras2d.Circunferencia import Circunferencia as Puerkito
 from figuras3d.Esfera import Esfera
-from figuras3d.Punto3D import Punto3D
+from figuras3d.Punto3D import Punto3D as Chanchito
 
 
 
-print(Punto())
-print(Circunferencia())
-print(Esfera(Punto3D(0, 0, 0), 1))
+print(Rosa())
+print(Puerkito())
+print(Esfera(Chanchito(0, 0, 0), 1))
 
 #...
